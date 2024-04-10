241b9264af3664f26b336c902861b86fecd1b2ed
TashdidDiganta <tashdiddiganta@gmail.com>
2024-04-10 13:09:57 +0600

    header responsive

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -37,82 +37,111 @@
                             </form>
                         </div>
                         <ul class="tc-cart d-flex">
-                            <li><a href="#" data-bs-toggle="offcanvas" data-bs-target="#useroffcanvasRight" aria-controls="offcanvasRight">
-                                <LazyIcon class="header-icon" name="ph:user-light" /></a></li>
-                            <li><a href="#">
-                                <LazyIcon class="header-icon" name="mdi:cards-heart-outline" /></a></li>
-                            <li><a href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                            <li><NuxtLink href="#" data-bs-toggle="offcanvas" data-bs-target="#useroffcanvasRight" aria-controls="offcanvasRight">
+                                <LazyIcon class="header-icon" name="ph:user-light" /></NuxtLink></li>
+                            <li><NuxtLink href="#">
+                                <LazyIcon class="header-icon" name="mdi:cards-heart-outline" /></NuxtLink></li>
+                            <li><NuxtLink href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
                                 <LazyIcon class="header-icon" name="ri:shopping-cart-line" />    
-                            <span>0</span></a></li>
+                            <span>0</span></NuxtLink></li>
                         </ul>
-                    </div>
-    
-                    <!-- shop canvas -->
-                    <div class="offcanvas offcanvas-end tc-shop-offcanvas" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-                        <div class="offcanvas-header tc-offcanvas-header justify-content-between">
-                            <h5 class="offcanvas-title" id="offcanvasRightLabel">Shopping Cart</h5>
-                            <button type="button"  data-bs-dismiss="offcanvas">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-                            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-                            </svg> Close</button>
-                        </div>
-                        <hr>
-                        <div class="offcanvas-body tc-offcanvas-body">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="86" height="86" fill="currentColor" class="bi bi-cart-x" viewBox="0 0 16 16">
-                                <path d="M7.354 5.646a.5.5 0 1 0-.708.708L7.793 7.5 6.646 8.646a.5.5 0 1 0 .708.708L8.5 8.207l1.146 1.147a.5.5 0 0 0 .708-.708L9.207 7.5l1.147-1.146a.5.5 0 0 0-.708-.708L8.5 6.793z"/>
-                                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
-                            </svg>
-                            <p>No products in the cart .</p>
-                            <a href="#" class="tc-btn-1">Return To Shop</a>
-                        </div>
-                    </div>
-    
-                    <!-- user canvas -->
-                    <div class="offcanvas offcanvas-end tc-shop-offcanvas" tabindex="-1" id="useroffcanvasRight" aria-labelledby="offcanvasRightLabel">
-                        <div class="offcanvas-header tc-offcanvas-header justify-content-between">
-                            <h5 class="offcanvas-title" id="offcanvasRightLabel">Sing In</h5>
-                            <button type="button"  data-bs-dismiss="offcanvas">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-                            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-                            </svg> Close</button>
-                        </div>
-                        <hr>
-                        <div class="offcanvas-body user-offcanvas">
-                            <form>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Username or email address *</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1">
-                                </div>
-                                <div class="mb-3 form-check">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    <label class="form-check-label" for="exampleCheck1"> Remember me</label>
-                                </div>
-                                <button type="submit" class="tc-btn-1">SING IN</button>
-                            </form>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="responsive-header d-none">
-            <div class="container d-flex justify-content-between ">
+            <div class="responsive-container d-flex justify-content-between ">
                 <div class="navigation-menu">
                     <button type="submit" class="search-submit">
                       <LazyIcon class="header-icon" name="material-symbols:menu-rounded" /> Menu
                     </button>
                 </div>
                 <div class="responsive-logo">
-                    <a href="#"><img src="../public/img/he-logo-green.png" alt=""></a>
+                    <NuxtLink href="#"><img src="../public/img/he-logo-green.png" alt=""></NuxtLink>
                 </div>
                 <div class="tc-cart">
-                    <li><a href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
-                        <LazyIcon class="header-icon" name="ri:shopping-cart-line" /><span>0</span></a>
+                    <li><NuxtLink href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                        <LazyIcon class="header-icon" name="ri:shopping-cart-line" /><span>0</span>
+                        </NuxtLink>
                     </li>
                 </div>
+            </div>
+        </div>
+        <div class="tc-responsive-toolbar">
+            <div class="container">
+                <div class="tc-cart toolbar d-flex ">
+                    <li><NuxtLink href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                        <LazyIcon class="header-icon" name="ion:storefront-outline" /><span>0</span>
+                        </NuxtLink>
+                    </li>
+                    <li><NuxtLink href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                        <LazyIcon class="header-icon" name="ri:shopping-cart-line" /><span>0</span>
+                        </NuxtLink>
+                    </li>
+                    <li><NuxtLink href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                        <LazyIcon class="header-icon" name="ri:shopping-cart-line" /><span>0</span>
+                        </NuxtLink>
+                    </li>
+                    <li><NuxtLink href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                        <LazyIcon class="header-icon" name="ri:shopping-cart-line" /><span>0</span>
+                        </NuxtLink>
+                    </li>
+
+
+                    <div class="toolbar-item">
+                        <NuxtLink href="#">
+                            <LazyIcon name="ion:storefront-outline" />
+                        </NuxtLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- shop canvas -->
+        <div class="offcanvas offcanvas-end tc-shop-offcanvas" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+            <div class="offcanvas-header tc-offcanvas-header justify-content-between">
+                <h5 class="offcanvas-title" id="offcanvasRightLabel">Shopping Cart</h5>
+                <button type="button"  data-bs-dismiss="offcanvas">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                </svg> Close</button>
+            </div>
+            <hr>
+            <div class="offcanvas-body tc-offcanvas-body">
+                <svg xmlns="http://www.w3.org/2000/svg" width="86" height="86" fill="currentColor" class="bi bi-cart-x" viewBox="0 0 16 16">
+                    <path d="M7.354 5.646a.5.5 0 1 0-.708.708L7.793 7.5 6.646 8.646a.5.5 0 1 0 .708.708L8.5 8.207l1.146 1.147a.5.5 0 0 0 .708-.708L9.207 7.5l1.147-1.146a.5.5 0 0 0-.708-.708L8.5 6.793z"/>
+                    <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                </svg>
+                <p>No products in the cart .</p>
+                <a href="#" class="tc-btn-1">Return To Shop</a>
+            </div>
+        </div>
+
+        <!-- user canvas -->
+        <div class="offcanvas offcanvas-end tc-shop-offcanvas" tabindex="-1" id="useroffcanvasRight" aria-labelledby="offcanvasRightLabel">
+            <div class="offcanvas-header tc-offcanvas-header justify-content-between">
+                <h5 class="offcanvas-title" id="offcanvasRightLabel">Sing In</h5>
+                <button type="button"  data-bs-dismiss="offcanvas">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                </svg> Close</button>
+            </div>
+            <hr>
+            <div class="offcanvas-body user-offcanvas">
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Username or email address *</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1"> Remember me</label>
+                    </div>
+                    <button type="submit" class="tc-btn-1">SING IN</button>
+                </form>
             </div>
         </div>
     </header>
@@ -333,6 +362,8 @@
 
 </style>
 <script>
+
+
 
 
 
