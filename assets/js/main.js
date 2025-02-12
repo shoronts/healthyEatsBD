2c74b75364b8e06b00df5934b69d31ee81648382
Ashrafull islam <mmmdashrafull211@gmail.com>
2025-02-12 20:19:28 +0600

    blog section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Sticky Header
-    document.addEventListener("scroll", () => {
-        const header = document.getElementById("header");
-        header.classList.toggle("scrolled", window.scrollY > 30);
-    });
-    // Home Rero Slider
-    const swiper = new Swiper(".mySwiper", {
-        loop: true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        autoplay: {
-          delay: 4000,
-          disableOnInteraction: false,
-        },
-        keyboard: true,
-      });
-      // card slider 
-      var Cardswiper = new Swiper(".myCardSwiper", {
-        slidesPerView: 5,
-        spaceBetween: 5,
-        loop: true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
+  // Sticky Header
+  document.addEventListener("scroll", () => {
+    const header = document.getElementById("header");
+    header.classList.toggle("scrolled", window.scrollY > 30);
+  });
+  // hero section 
+  const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 2,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  // card slider 
+  var Cardswiper = new Swiper(".myCardSwiper", {
+    slidesPerView: 5,
+    spaceBetween: 5,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  // review slider 
+  const reviewSwiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
